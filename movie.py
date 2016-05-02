19f789d71b651fd5d947ef9c919b3ecd5e3841f5
NakanishiTetsuhiro <kaoru6strings@gmail.com>
2016-05-02 22:06:01 +0900

    Scraping to Transformer

--- movie.py
+++ movie.py
@@ -17,48 +17,28 @@ def get_web_esource(url):
         return None
 
 
-def get_movie_title_and_img(tag_movie_li, f):
-    movie_list = []
-    for movieDetail in tag_movie_li:
-
-        try:
-            # titleを取得
-            unnormalize_title = str(movieDetail.getText()).strip()
-            replace_title = re.sub('(【.+|\\n).+', '', unnormalize_title)
-            title = replace_title.strip()
-
-            # imgを取得
-            img = movieDetail.find('div', {'class': 'pict'}).a.img
-            img_src = img['src']
-            big_img_url = re.sub('\.jpg$', '1.jpg', img_src)
-            movie_list.append({
-                'title': title,
-                'url': big_img_url
-            })
-            f.write(str(title) + ',' + str(big_img_url) + '\n')
-        except AttributeError as e:
-            print(e)
-
 def main():
-    albatross_url = 'http://www.albatros-film.com/archives/category/dvd/page/'
-    page = 1
+    transformer_url = 'http://www.transformer.co.jp/products/index.html'
     try:
-        f = open('albatross.csv', mode='a', encoding='utf8')
+        f = open('transformer.csv', mode='a', encoding='utf8')
 
-        while(page < 10):
-            resource = get_web_esource(albatross_url + str(page))
-            read_resource = resource.read()
-            bs_obj = BeautifulSoup(read_resource, 'lxml')
-            tag_movie_li = bs_obj.find('div', {'class': 'bg'}).ul.findAll('li')
-            get_movie_title_and_img(tag_movie_li, f)
-            time.sleep(1)
-            page = page + 1
+        resource = get_web_esource(transformer_url)
+        read_resource = resource.read()
+        bs_obj = BeautifulSoup(read_resource, 'lxml')
+
+        img_tags = bs_obj.findAll('img', class_='ph')
+
+        for img_tag in img_tags:
+            f.write(str(img_tag['alt']) + ',' + str(img_tag['src']) + '\n')
+            # print(img_tag['src'])
+
     except AttributeError as e:
         print(e)
     except IOError as e:
         print(e)
 
     f.close()
+
 
 if __name__ == '__main__':
     main()
